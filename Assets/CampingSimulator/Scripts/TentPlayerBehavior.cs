dd1177b231c75a2e24c6d11d8b512ad14851a1d9
Adam Veldhousen <adam@veldhousen.ninja>
2015-08-04 22:52:05 -0500

    trimmed down mouse/fp camera bs to a couple of scripts

---
+++ Assets/CampingSimulator/Scripts/TentPlayerBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Assets.CampingSimulator
+{
+
+    public class TentPlayerBehavior : MonoBehaviour
+    {
+
+        [SerializeField]
+        private MouseLook MouseLook;
+        [SerializeField]
+        private Camera FPCamera;
+
+        // Use this for initialization
+        void Start()
+        {
+            FPCamera = Camera.main;
+            MouseLook.Init(transform, FPCamera.transform);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            MouseLook.LookRotation(transform, FPCamera.transform);
+        }
+    }
+}
